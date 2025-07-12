a2480f98d7e1e8b16d7f3e806dca62607c4a122a
David Villafaña <dvillafanaiv@proton.me>
2025-07-12 02:58:17 -0500

    feat: use upstream orgparse

--- flake.nix
+++ flake.nix
@@ -14,34 +14,8 @@
         flake-utils.lib.eachDefaultSystem (system:
             let
                 pkgs = nixpkgs.legacyPackages.${system};
-                python-env = pkgs.python312.withPackages(ps: with ps; [
-                    (pkgs.python312Packages.buildPythonPackage {
-                        pname = "orgparse";
-                        version = "0.1.0";
-                        src = pkgs.fetchFromGitHub {
-                            owner = "dtvillafana";
-                            repo = "orgparse";
-                            rev = "master";
-                            sha256 = "sha256-R2IHJ7WUoqH+T/nuhOI8Ek9GKAGib3JSE3FUYDjnv5E=";
-                        };
-                        pyproject = true;
-                        propagatedBuildInputs = with pkgs.python312Packages; [
-                            setuptools
-                            setuptools-scm
-                        ];
-                        checkInputs = with pkgs.python312Packages; [
-                            pytest
-                            ruff
-                            mypy
-                            lxml
-                        ];
-                        doCheck = true; # Enable if you have tests
-                        meta = with pkgs.lib; {
-                            description = "Emacs org-mode parser in Python";
-                            homepage = "https://github.com/dtvillafana/orgparse";
-                            license = licenses.bsd3;
-                        };
-                    })
+                python-env = pkgs.python313.withPackages(ps: with ps; [
+                    orgparse
                     requests
                     dateutil
                     pytest
@@ -83,7 +57,7 @@
                                         etc."org-notifier/service-vars" = {
                                             # Source path is relative to the Nix store
                                             text = ''NTFY_URL="${env-vars.NTFY_URL}"
-ORG_BASEDIR="/home/master/git-repos/orgfiles"'';
+ORG_BASEDIR="/home/master/git-repos/orgfiles/"'';
                                             # Set appropriate permissions
                                             mode = "0600";
                                             user = "master";
